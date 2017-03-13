14cbe3a402cc1f4c1a978c35095e76d70917999b
ErikBoesen <me@erikboesen.com>
2017-03-13 17:14:40 -0400

    Starting app. Will work fine once shirt is up online.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # shrt
-CLI client for creating links with shrtn
+CLI client for creating links with [shirt](https://github.com/ErikBoesen/shirt).
+
+## Setup
+Set your default host by adding a `$SHRT_HOST` environment variable:
+
+    export SHRT_HOST=https://myshortener.com
---
+++ shrt.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import os
+import requests
+import argparse
+import json
+import re
+import pyperclip
+from termcolor import colored
+
+parser = argparse.ArgumentParser(description='Create shortlinks with shirt.')
+parser.add_argument('url', type=str, help='The URL you want to shorten.')
+parser.add_argument('--key', dest='key', default=None, help='Custom key to use.')
+parser.add_argument('--host', dest='host', default=os.environ['SHRT_HOST'], help='A custom host to upload links to, if necessary. Defaults to $SHRT_HOST.')
+
+args = parser.parse_args()
+
+data = {'url': args.url}
+
+if args.key is not None:
+    data['key'] = args.key
+
+r = requests.post('%s/new' % args.host, headers={'Content-Type': 'application/json'}, data=json.dumps(data))
+
+resp = r.json()
+
+if resp.get('error') is not None:
+    print('The key %s is already taken.' % args.key)
+else:
+    url = '%s/%s' % (args.host, resp['key'])
+    print('\n    ' + colored('✓', 'green') + ' Shortlink accessible at %s (copied.)\n' % colored(re.sub(r'^https?://(www\.)?', '', url), 'cyan'))
+    pyperclip.copy(url)
